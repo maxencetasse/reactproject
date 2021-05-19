bf33cfa778527ac857232143df19907d0a731df8
Maxence Tasse <mtasse@osi-sante.fr>
2021-05-19 18:00:33 +0200

    Changements sur les hooks

--- Chapitre 3 (Hooks)/exercice-hooksapercu/src/index.js
+++ Chapitre 3 (Hooks)/exercice-hooksapercu/src/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Example() {
+  const [count, setCount] = useState(0);
+
+  // Équivalent à componentDidMount plus componentDidUpdate :
+  useEffect(() => {
+    // Mettre à jour le titre du document en utilisant l'API du navigateur
+    document.title = `Bouton cliqué ${count} fois`;
+  });
+
+  return (
+    <div>
+      <p>Bouton cliqué {count} fois</p>
+      <button onClick={() => setCount(count + 1)}>
+        Cliquez ici
+      </button>
+    </div>
+  );
+}
+
+const [age, setAge] = useState(42);
+const [fruit, setFruit] = useState('banane');
+const [todos, setTodos] = useState([{ text: 'Apprendre les Hooks' }]);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Example></Example>,
   document.getElementById('root')
+);
+
+ReactDOM.render(
+  <ExampleFruit></ExampleFruit>,
+  document.getElementById('exemplefruit')
 );
 
 // If you want to start measuring performance in your app, pass a function
